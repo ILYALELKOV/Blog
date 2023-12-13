ff6ea8223aa441d7deb98ee70ca406fd775a027f
Ilya Lelkov <ilya.lelkov@mail.ru>
2023-12-13 20:12:32 +1000

    create authorization

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,5 @@
-import PropTypes from 'prop-types'
 import { ControlPanel, Logo } from './components'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
 const Discription = styled.div`
---
+++ src/store.js
@@ -0,0 +1,15 @@
+import { compose, createStore, applyMiddleware, combineReducers } from 'redux'
+import { appReducer, postReducer, postsReducer, userReducer, usersReducer } from './reducers'
+import { thunk } from 'redux-thunk'
+
+const reducer = combineReducers({
+	app: appReducer,
+	user: userReducer,
+	users: usersReducer,
+	post: postReducer,
+	posts: postsReducer
+})
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose()
+
+export const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
